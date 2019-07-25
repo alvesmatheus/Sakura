c97a7349287b8a46c59c9d0bb6295b81cc3da568
SpinnelSun <m.hollygard@gmail.com>
2019-07-25 19:53:03 -0300

    Add Clow Cards xpath selectors

---
+++ crawlers/spiders/selectors/clow_cards_selectors.py
@@ -0,0 +1,11 @@
+CARD_PAGE = '//div[@class="lightbox-caption"]/a/@href'
+
+NAME = '//table[@class="infobox"]//td/text()'
+
+IMAGE_URL = '//div[@title="Clow Card"]//a/@href'
+
+SIGN = '//table[@class="infobox"]//tr[3]//a/text() | //table[@class="infobox"]//tr[3]/td[2]/text()'
+
+HIEGHERARCHY = '//table[@class="infobox"]//tr[4]//a/text() | //table[@class="infobox"]//tr[4]/td[2]/text()'
+
+MAGIC_TYPE = '//table[@class="infobox"]//tr[5]/td[2]/text()'
