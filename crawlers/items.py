90eb72711fd1c12971743255b3d56f1e0ce33593
SpinnelSun <m.hollygard@gmail.com>
2019-07-25 19:52:29 -0300

    Add Items and Loaders to Clow Cards

---
+++ crawlers/items.py
@@ -0,0 +1,9 @@
+import scrapy
+
+class ClowCardItem(scrapy.Item):
+    card = scrapy.Field()
+    card_type = scrapy.Field()
+    img_url = scrapy.Field()
+    sign = scrapy.Field()
+    hierarchy = scrapy.Field()
+    magic_type = scrapy.Field()
---
+++ crawlers/loaders.py
@@ -0,0 +1,21 @@
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst
+
+def fix_text_field(self, values):
+    text = values[0]
+    return text.replace('\n', '')
+    
+def fix_url_field(self, values):
+    url = values[0]
+    return url.split('/revision')[0]
+
+class ClowCardLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+
+    card_out = fix_text_field
+    sign_out = fix_text_field
+    hierarchy_out = fix_text_field
+    magic_type_out = fix_text_field
+
+    img_url_out = fix_url_field
+
