a042ac4058230093f07fb47a6dbb275648ba68e5
Aaron Gussman <agussman@gmail.com>
2019-10-16 22:47:52 -0400

    #1 Crawl Sakura cards. Edited all xpath selectors to match new page format (Note: existing clow spider is probably broken)

--- crawlers/loaders.py
+++ crawlers/loaders.py
@@ -19,3 +19,12 @@ class ClowCardLoader(ItemLoader):
 
     img_url_out = fix_url_field
 
+class SakuraCardLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+
+    card_out = fix_text_field
+    sign_out = fix_text_field
+    hierarchy_out = fix_text_field
+    magic_type_out = fix_text_field
+
+    img_url_out = fix_url_field
